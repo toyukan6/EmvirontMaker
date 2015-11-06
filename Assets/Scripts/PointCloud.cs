9b765738932f344eb003191d60904cb7393ff28c
toyukan <toyukan@kmc.gr.jp>
2015-11-06 13:33:49 +0900

    index.csvを使うように変更

--- Assets/Scripts/PointCloud.cs
+++ Assets/Scripts/PointCloud.cs
@@ -10,33 +10,36 @@ namespace EnvironmentMaker {
     public class PointCloud : MonoBehaviour {
 
         PlyReader reader;
-        List<Vector3[]> walkPoints;
-        List<Color[]> walkColors;
+        List<Vector3[][]> walkPoints;
+        List<Color[][]> walkColors;
         List<Vector3> walkHeadPos;
-        List<Vector3[]> jojoPoints;
-        List<Color[]> jojoColors;
-        List<Vector3> jojoHeadPos;
         List<Vector3> bodyposes;
         bool walk = true;
+        int dotNums = 4;
 
         private Mesh mesh;
 
-        int pointsNumber = 0;
+        int[] beforeTime;
+        int[] pointsNumbers;
+        List<MyTime[]> fileTimes;
 
         // Use this for initialization
         void Start() {
             mesh = new Mesh();
             reader = new PlyReader();
-            walkPoints = new List<Vector3[]>();
-            walkColors = new List<Color[]>();
+            walkPoints = new List<Vector3[][]>();
+            walkColors = new List<Color[][]>();
+            fileTimes = new List<MyTime[]>();
             walkHeadPos = new List<Vector3>();
-            jojoPoints = new List<Vector3[]>();
-            jojoColors = new List<Color[]>();
-            jojoHeadPos = new List<Vector3>();
-
+            beforeTime = new int[dotNums];
+            pointsNumbers = new int[dotNums];
+            for (int i = 0; i < pointsNumbers.Length; i++) {
+                pointsNumbers[i] = 0;
+            }
             GetComponent<MeshFilter>().mesh = mesh;
-            LoadModels("result", walkPoints, walkColors);
-            LoadModels("difficult", jojoPoints, jojoColors);
+            string dir = "result";
+            LoadModels(dir, walkPoints, walkColors);
+            LoadIndexCSV(dir);
         }
 
         void Update() {
@@ -46,26 +49,36 @@ namespace EnvironmentMaker {
         }
 
         void FixedUpdate() {
-            //Destroy(GetComponent<MeshFilter>().mesh);
+            Destroy(GetComponent<MeshFilter>().mesh);
             mesh = new Mesh();
             GetComponent<MeshFilter>().mesh = mesh;
 
-            var points = (walk) ? walkPoints : jojoPoints;
-            var colors = (walk) ? walkColors : jojoColors;
-            pointsNumber %= points.Count;
-            mesh.vertices = points[pointsNumber];
-            mesh.colors = colors[pointsNumber];
-            mesh.SetIndices(Enumerable.Range(0, points[pointsNumber].Length).ToArray(), MeshTopology.Points, 0);
-            pointsNumber = (pointsNumber + 1) % walkPoints.Count;
+            var points = new List<Vector3>();
+            var colors = new List<Color>();
+            var time = Time.deltaTime * 1000;
+            for (int i = 0; i < dotNums; i++) {
+                beforeTime[i] += (int)Math.Floor(time);
+                if (beforeTime[i] > fileTimes[pointsNumbers[i] + 1][i].GetMilli() - fileTimes[pointsNumbers[i]][i].GetMilli())
+                    pointsNumbers[i] = (pointsNumbers[i] + 1) % walkPoints.Count;
+                foreach (var v in walkPoints[pointsNumbers[i]][i]) {
+                    points.Add(v);
+                }
+                foreach (var c in walkColors[pointsNumbers[i]][i]) {
+                    colors.Add(c);
+                }
+            }
+            mesh.vertices = points.ToArray();
+            mesh.colors = colors.ToArray();
+            mesh.SetIndices(Enumerable.Range(0, points.Count).ToArray(), MeshTopology.Points, 0);
         }
 
-        void LoadModels(string dir, List<Vector3[]> points, List<Color[]> colors) {
+        void LoadModels(string dir, List<Vector3[][]> points, List<Color[][]> colors) {
             string baseDir = Path.Combine("polygons", dir);
             int num = 1;
             while (File.Exists(Path.Combine(baseDir, "model_" + num + "_0.ply"))) {
-                var vecs = new LinkedList<Vector3>();
-                var cols = new LinkedList<Color>();
-                for (int i = 0; i < 4; i++) {
+                var vllist = new LinkedList<Vector3>[dotNums];
+                var cllist = new LinkedList<Color>[dotNums];
+                for (int i = 0; i < dotNums; i++) {
                     var plist = new List<Point>();
                     var vlist = new LinkedList<Vector3>();
                     var clist = new LinkedList<Color>();
@@ -78,23 +91,59 @@ namespace EnvironmentMaker {
                         vlist.AddLast(p.GetVector3());
                         clist.AddLast(p.GetColor());
                     }
-                    float minY = 0, minListY = 0;
-                    var slices = RoundSlices(vecs);
-                    if (vecs.Count > 0) {
-                        minY = vecs.Min(v => v.y);
-                        minListY = vlist.Min(v => v.y);
-                    }
-                    foreach (var v in vlist) {
-                        vecs.AddFirst(v + new Vector3(0, minY - minListY, 0));
-                    }
-                    foreach (var c in clist) {
-                        cols.AddFirst(c);
-                    }
+                    var slice = RoundSlices(vlist);
+                    vllist[i] = vlist;
+                    cllist[i] = clist;
+                    //foreach (var v in vlist) {
+                    //    vecs.AddFirst(v + new Vector3(0, minY - minListY, 0));
+                    //}
+                    //foreach (var c in clist) {
+                    //    cols.AddFirst(c);
+                    //}
                 }
-                points.Add(vecs.ToArray());
-                colors.Add(cols.ToArray());
+                Vector3[][] varray = new Vector3[dotNums][];
+                Color[][] carray = new Color[dotNums][];
+                for (int i = 0; i < vllist.Length; i++) {
+                    varray[i] = vllist[i].ToArray();
+                }
+                for (int i = 0; i < cllist.Length; i++) {
+                    carray[i] = cllist[i].ToArray();
+                }
+                points.Add(varray);
+                colors.Add(carray);
                 num++;
             }
+        }
+
+        void LoadIndexCSV(string dir) {
+            string baseDir = Path.Combine("polygons", dir);
+            List<string[]> data = new List<string[]>();
+            using (StreamReader reader = new StreamReader(Path.Combine(baseDir, "index.csv"))) {
+                string str = reader.ReadLine();
+                while(str != null) {
+                    var split = str.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                    if (split.Length > 0)
+                        data.Add(split);
+                    str = reader.ReadLine();
+                }
+            }
+            for (int i = 0; i < data.Count; i += dotNums) {
+                var times = new MyTime[dotNums];
+                for (int j = 0; j < dotNums; j++) {
+                    times[j] = ParseTime(data[i][1]);
+                }
+                fileTimes.Add(times);
+            }
+        }
+
+        MyTime ParseTime(string str) {
+            var split = str.Split(new[] { ":" }, StringSplitOptions.RemoveEmptyEntries);
+            int hour, minute, second, millisecond;
+            if (!int.TryParse(split[0], out hour)) return null;
+            if (!int.TryParse(split[1], out minute)) return null;
+            if (!int.TryParse(split[2], out second)) return null;
+            if (!int.TryParse(split[3], out millisecond)) return null;
+            return new MyTime(hour, minute, second, millisecond);
         }
 
         void LoadBody(string dir) {
@@ -118,10 +167,13 @@ namespace EnvironmentMaker {
 
         List<List<Vector3>> RoundSlices(LinkedList<Vector3> vecs) {
             var slices = new List<List<Vector3>>();
-            for (int i = 0; i < vecs.Count; i++) {
+            var used = new List<Vector3>();
+            for (int i = 0; i < vecs.Count;) {
                 var baseVec = vecs.ElementAt(i);
-                var take = vecs.TakeWhile(v => baseVec.y - v.y < 0.01);
+                var take = vecs.TakeWhile(v => baseVec.y - v.y < 0.1).ToList();
+                used.ForEach(u => take.Remove(u));
                 foreach (var t in take) {
+                    used.Add(t);
                     i++;
                 }
                 slices.Add(take.ToList());
